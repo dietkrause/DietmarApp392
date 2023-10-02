5c118cc86e3ad2c2b45c622456034578aacf731b
dietkrause <dietmarkrause2025@u.northwestern.edu>
2023-10-02 11:24:09 -0500

    style the app

---
+++ src/components/CourseCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+// Styling for the card
+const cardStyle = {
+  border: '1px solid #e0e0e0',
+  borderRadius: '5px',
+  padding: '15px',
+  margin: '10px 0'
+};
+
+const titleStyle = {
+  fontSize: '1.5rem',
+  fontWeight: 'bold',
+  marginBottom: '10px'
+};
+
+const contentStyle = {
+  fontSize: '1rem',
+  marginBottom: '15px'
+};
+
+const footerStyle = {
+  borderTop: '1px solid #e0e0e0',
+  marginTop: '10px',
+  paddingTop: '10px',
+  fontSize: '0.9rem'
+};
+
+const CourseCard = ({ title, content, footer }) => {
+  return (
+    <div style={cardStyle}>
+      <div style={titleStyle}>{title}</div>
+      <div style={contentStyle}>{content}</div>
+      <div style={footerStyle}>{footer}</div>
+    </div>
+  );
+}
+
+export default CourseCard;
--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,31 +1,29 @@
 import React from 'react';
-
+import CourseCard from './CourseCard';
 
 const CourseList = ({ schedule }) => {
+  const containerStyle = {
+    overflowX: 'auto',
+    whiteSpace: 'nowrap',
+    padding: '10px 0'
+  };
+
+  const cardWrapperStyle = {
+    display: 'inline-block',
+    marginRight: '15px'
+  };
+
   return (
-    <div className="App-body">
-        <center>
-        <table>
-          <thead>
-            <tr>
-              <th>Term</th>
-              <th>Code</th>
-              <th>Course</th>
-              <th>When</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.entries(schedule.courses).map(([id, course]) => 
-              <tr key={id}>
-                <td>{course.term}</td>
-                <td>CS {course.number}</td>
-                <td>{course.title}</td>
-                <td>{course.meets}</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-        </center>
+    <div className="App-body" style={containerStyle}>
+      {Object.entries(schedule.courses).map(([id, course]) => (
+        <div key={id} style={cardWrapperStyle}>
+          <CourseCard 
+            title={`${course.term} CS ${course.number}`}
+            content={course.title}
+            footer={course.meets}
+          />
+        </div>
+      ))}
     </div>
   );
 }
