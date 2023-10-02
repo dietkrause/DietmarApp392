68b393b167d4c5c85aa5f91e13068cec7cec2d11
dietkrause <dietmarkrause2025@u.northwestern.edu>
2023-10-02 10:42:22 -0500

    display list of courses

--- src/App.js
+++ src/App.js
@@ -40,22 +40,28 @@ function App() {
         <h1>{schedule.title}</h1>
       </header>
       <div className="App-body">
+        <center>
         <table>
           <thead>
             <tr>
-              <th>Course</th>
               <th>Term</th>
+              <th>Code</th>
+              <th>Course</th>
+              <th>When</th>
             </tr>
           </thead>
           <tbody>
             {Object.entries(schedule.courses).map(([id, course]) => 
               <tr key={id}>
+                <td>{course.term}</td>
+                <td>CS {course.number}</td>
                 <td>{course.title}</td>
                 <td>{course.meets}</td>
               </tr>
             )}
           </tbody>
         </table>
+        </center>
     </div>
     </div>
   );
