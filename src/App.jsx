e4b14d4ca82e65f6ee0278e489743855c2e3dae7
dietkrause <dietmarkrause2025@u.northwestern.edu>
2023-10-09 11:04:53 -0500

    data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -3,77 +3,25 @@ import './App.css';
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
 import React, { useState, useEffect } from 'react';
+import useFetching from './utilities/fetching';
 
-
-const schedule = {
-  "title": "CS Courses for 2023-2024",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
 
 function App() {
-  const [data, setCourses] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setCourses(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error);
-        setLoading(false);
-      });
-  }, []);
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
+  const url = "https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php";
+  const result = useFetching(url)
+  
+  if (result.status === 0 || result.status == 1) {
+    return <div>{result.message}</div>;
   }
   return (
     <div>
       <div className="App">
-      <Banner content={schedule.title} />
-      <CourseList schedule={data}/>
+      <Banner content={result.data.title} />
+      <CourseList schedule={result.data}/>
       </div>
     </div>
 
   );
 }
-
 
 export default App;
---
+++ src/utilities/fetching.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+
+function useFetching(url) {
+    // status 0: loading
+    // status 1: error
+    // status 2: success
+  
+    const [data, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const result = {"status":0,"data":null,"message":""};
+  
+    useEffect(() => {
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setCourses(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError(error);
+          setLoading(false);
+        });
+    }, [url]);
+  
+    if (loading) {
+      result.status = 0;
+      result.message = "Loading..."
+      return result;
+    }
+  
+    if (error) {
+      result.status = 1
+      result.message = error.message
+      return result;
+    }
+    result.status = 2;
+    result.data = data;
+    result.message = "200 OK"
+    return result;
+  }
+  
+  export default useFetching;
