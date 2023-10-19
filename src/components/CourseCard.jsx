bea1f3752932839bbc48f1cc9bb8949422c11857
dietkrause <dietmarkrause2025@u.northwestern.edu>
2023-10-18 19:09:59 -0500

    only verified users can edit

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -3,19 +3,26 @@ import './CourseCard.css';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { useAuthState } from '../utilities/firebase'; // Import the useAuthState hook
 
+const CourseCard = ({ title, content, footer, isSelected, onClick, overlappingWithSelected, id }) => {
+  const [user] = useAuthState(); // Get the user authentication state
 
-const CourseCard = ({ title, content, footer, isSelected, onClick, overlappingWithSelected,id }) => {
-    return (
-        <div className={`card ${isSelected ? 'selected' : overlappingWithSelected ? 'overlapping' : ''}`} onClick={onClick}>
-            <Link className="edit-button" to={`/edit/${id}`}>
-            <FontAwesomeIcon icon={faPencilAlt} />
-            </Link>
-            <div className="title">{title}</div>
-            <div className="content">{content}</div>
-            <div className="footer">{footer}</div>
-        </div>
-    );
+  return (
+    <div className={`card ${isSelected ? 'selected' : overlappingWithSelected ? 'overlapping' : ''}`} onClick={onClick}>
+      {user ? ( // Check if the user is logged in
+        <Link className="edit-button" to={`/edit/${id}`}>
+          <FontAwesomeIcon icon={faPencilAlt} />
+        </Link>
+      ) : (
+        // Render something else or nothing if the user is not logged in
+        null
+      )}
+      <div className="title">{title}</div>
+      <div className="content">{content}</div>
+      <div className="footer">{footer}</div>
+    </div>
+  );
 };
 
 export default CourseCard;
