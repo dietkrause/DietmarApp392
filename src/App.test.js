f062c72d9a6ce846be2e681e2b0a32c8a2db8371
dietkrause <dietmarkrause2025@u.northwestern.edu>
2023-11-15 15:26:36 -0600

    update to vitest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
+import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+
+  it('renders without crashing', () => {
+    render(<App />);
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+  });
+
+  it('renders the course plan button', () => {
+    render(<App />);
+    const buttonElement = screen.getByText(/course plan/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+
 });
